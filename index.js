c3eafcbba839825c83b10d02193df5993e68ae13
Ryan Mason <ryanmasonmusic@gmail.com>
2022-12-03 14:43:56 -0500

    commit tree 642b82549d5695f686e508371c3df9cb259cd8c3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+
+
+function findMatching(drivers, string){
+    return drivers.filter(
+    (findMatching) => findMatching.toLowerCase() === string.toLowerCase()
+    );
+    }
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(
+        (name) => name.indexOf(string) === 0
+    );
+}
+
+function matchName(drivers, string){
+    // console.log("does this console log" , drivers.name)
+    return drivers.filter(
+       driver => driver.name === string
+    );
+}
